06c12735ffa413b5224f76ee7599323616523602
Deyson <deyson.trindade@smilego.com.br>
2022-03-09 21:56:22 -0300

    initial commit

---
+++ src/main/java/br/com/deyson/vuttr/VuttrApplication.java
@@ -0,0 +1,13 @@
+package br.com.deyson.vuttr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuttrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuttrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/deyson/vuttr/VuttrApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.deyson.vuttr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VuttrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
